570de89bee6485ccd5f7f5848c5169d74fd891a8
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-17 18:28:43 +0100

    Add JourneyPage with participant search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import DefaultLayout from "./layouts/DefaultLayout"
 import Homepage from "./pages/Homepage"
+import JourneyPage from "./pages/JourneyPage"
 
 function App() {
 
--- src/pages/JourneyPage.jsx
+++ src/pages/JourneyPage.jsx
@@ -1,5 +1,44 @@
+import { useParams } from "react-router-dom"
+import journeysData from "../assets/journeysData"
+import { useEffect, useState } from "react"
+
 export default function JourneyPage() {
+
+    const [data, setData] = useState(journeysData)
+    const [search, setSearch] = useState('')
+    const { id } = useParams()
+
+    const current = data.find(current => Number(id) === current.id)
+
+    const filteredPartecipanti = current.partecipanti.filter(partecipante => {
+        const fullName = `${partecipante.nome} ${partecipante.cognome}`.toLowerCase()
+        return fullName.includes(search.toLowerCase())
+    })
+
     return (
-    
-   )
+        <section className="journey">
+            <div className="container">
+
+                <input
+                    className="form-control my-3"
+                    type="text"
+                    placeholder="Cerca per nome e cognome..."
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)} />
+
+                <div className="row row-cols-2">
+
+                    {filteredPartecipanti.map(user => (
+                        <div key={user.id} className="col">
+                            <div className="card mt-3">
+                                <h6>{user.nome}</h6>
+                                <h6>{user.cognome}</h6>
+                            </div>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+        </section>
+    )
 }
