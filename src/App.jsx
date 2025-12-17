0bfefa57f79e91196532d0e9337244422ba5d7e7
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-17 15:39:11 +0100

    Add React Router and base layout Added react-router-dom as a dependency in package-lock.json Created the DefaultLayout component to handle the base layout structure Added the Homepage component as an example page Configured basic routing in the main application

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
 
 function App() {
 
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={DefaultLayout}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
