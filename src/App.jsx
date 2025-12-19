5e163460d86760dcebc7e7a6b7bf478475928d23
stefanobellomo <stefanobellomo1998@gmail.com>
2025-12-19 10:36:44 +0100

    fix and syncronyze code with group

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import DefaultLayout from "./layouts/DefaultLayout"
-import HomePage from "./pages/HomePage"
-import JourneyPage from "./pages/JourneyPage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import DefaultLayout from './layouts/DefaultLayout'
+import HomePage from './pages/HomePage'
+import JourneyPage from './pages/JourneyPage'
 
 function App() {
 
